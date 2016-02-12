4b45c006d5df3e7a9a4e8024b73ac854b7132b30
lawton <lawton.a.browning@gmail.com>
2016-02-12 10:49:19 -0800

    fixed errors and added working front end

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -13,49 +13,17 @@ namespace Address
         return View["index.cshtml"];
       };
       Get["/contacts"] = _ => {
-        var allContacts = Contact.GetAll();
+        List<Contact> allContacts = Contact.GetAll();
         return View["contacts.cshtml", allContacts];
       };
       Get["/contacts/new"] = _ => {
         return View["contact_form.cshtml"];
       };
       Post["/contact_added"] = _ => {
-        int intPhoneNumber = int.Parse(Request.Form["new-phoneNumber"])
+        int intPhoneNumber = int.Parse(Request.Form["new-phoneNumber"]);
         var newContact = new Contact(Request.Form["new-name"], Request.Form["new-address"],intPhoneNumber);
         var allContacts = Contact.GetAll();
         return View["contact_created.cshtml"];
-      };
-      Get["/categories/{id}"] = parameters => {
-        Dictionary<string, object> model = new Dictionary<string, object>();
-        var selectedCategory = Category.Find(parameters.id);
-        var categoryTasks = selectedCategory.GetTasks();
-        model.Add("category", selectedCategory);
-        model.Add("tasks", categoryTasks);
-        return View["category.cshtml", model];
-      };
-      Get["/categories/{id}/tasks/new"] = parameters => {
-        Dictionary<string, object> model = new Dictionary<string, object>();
-        Category selectedCategory = Category.Find(parameters.id);
-        List<Task> alltasks = selectedCategory.GetTasks();
-        model.Add("category", selectedCategory);
-        model.Add("tasks", alltasks);
-        return View["category_tasks_form.cshtml", model];
-      };
-      Post["/tasks"] = _ => {
-        Dictionary<string, object> model = new Dictionary<string, object>();
-        Category selectedCategory = Category.Find(Request.Form["category-id"]);
-        List<Task> categoryTasks =  selectedCategory.GetTasks();
-        string taskDescription = Request.Form["task-description"];
-        Task newTask = new Task(taskDescription);
-        categoryTasks.Add(newTask);
-        model.Add("tasks", categoryTasks);
-        model.Add("category", selectedCategory);
-        return View["category.cshtml", model];
-      };
-      Post["/categories/{id}"] = parameters => {
-        Category selectedCategory = Category.Find(parameters.id);
-        selectedCategory.Clear();
-        return View["tasks_deleted.cshtml", selectedCategory];
       };
     }
   }
--- Objects/AddressBook.cs
+++ Objects/AddressBook.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace Address.Objects
@@ -9,52 +8,57 @@ namespace Address.Objects
     private string _Address;
     private int _PhoneNumber;
     private int _Id;
-    private List<Contact> _instances = new List<Contact> {};
+    private static List<Contact> _instances = new List<Contact> {};
 
     public Contact(string Name, string Address, int PhoneNumber)
     {
     _Name = Name;
     _Address = Address;
     _PhoneNumber = PhoneNumber;
-    _Id = _instances.Count;
     _instances.Add(this);
+    _Id = _instances.Count;
     }
+
     public string GetName()
     {
       return _Name;
     }
-    public void SetName(string Name)
+
+    public void SetName(string newName)
     {
-      _Name = Name;
+      _Name = newName;
     }
+
     public string GetAddress()
     {
       return _Address;
     }
-    public void SetAddress(string Address)
+
+    public void SetAddress(string newAddress)
     {
-      _Address = Address;
+      _Address = newAddress;
     }
+
     public int GetPhoneNumber()
     {
       return _PhoneNumber;
     }
-    public void SetPhoneNumber(int PhoneNumber)
+
+    public void SetPhoneNumber(int newPhoneNumber)
     {
-      _PhoneNumber = PhoneNumber;
+      _PhoneNumber = newPhoneNumber;
     }
+
     public static List<Contact> GetAll()
     {
       return _instances;
     }
-    public void Save();
-    {
-      _instances.Add(this);
-    }
-    public void Clear()
+
+    public static void ClearAll()
     {
       _instances.Clear();
     }
+
     public static Contact Find(int searchId)
     {
       return _instances[searchId-1];
