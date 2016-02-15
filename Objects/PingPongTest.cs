a928b6dccbd8ff267247a9907676f6dfea9c9c4c
names <names>
2016-02-15 15:38:08 -0800

    close to specs passing

--- Objects/PingPongTest.cs
+++ Objects/PingPongTest.cs
@@ -1,36 +1,25 @@
-// using Xunit;
-// using System;
-// using System.Collections.Generic;
-// using PingPong;
-//
-// namespace PingPongTest
-// {
-//   public class PingPongTest
-//   {
-//     //arrange
-//     [Fact]
-//     public void IsPingPong_ForNumberDivisibleBy3_true()
-//     {
-//       PingPongClass testPingPongTest = new PingPongClass();
-//       Assert.Equal("ping", testPingPongTest.PingPonger(3));
-//     }
-//     [Fact]
-//     public void IsPingPong_ForNumberDivisibleBy5_true()
-//     {
-//       PingPongClass testPingPongTest = new PingPongClass();
-//       Assert.Equal("pong", testPingPongTest.PingPonger(5));
-//     }
-//     [Fact]
-//     public void IsPingPong_ForNumberDivisibleBy15_true()
-//     {
-//       PingPongClass testPingPongTest = new PingPongClass();
-//       Assert.Equal("ping-pong", testPingPongTest.PingPonger(15));
-//     }
-//     [Fact]
-//     public void IsPingPong_ForNumberDivisibleBy4_4()
-//     {
-//       PingPongClass testPingPongTest = new PingPongClass();
-//       Assert.Equal("4", testPingPongTest.PingPonger(4));
-//     }
-//   }
-// }
+using Xunit;
+using System;
+using System.Collections.Generic;
+using PingPong;
+
+namespace PingPongTest
+{
+  public class PingPongTester
+  {
+    [Fact]
+    public void ListOfNumbersUpToThree()
+    {
+    //arrange
+    int userint = 3;
+    PingPongClass example = new PingPongClass(userint);
+    List<string> userlist = PingPongClass.getOutput();
+
+    //act
+    List<string> testlist = new List<string>{"1","2","ping"};
+
+    //assert
+    Assert.Equal(testlist, userlist);
+    }
+  }
+}
